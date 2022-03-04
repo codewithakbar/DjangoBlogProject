e44c15092567e97f78fdf4510c30a9b8871966ae
AkbarAli <88106359+DataAkbar@users.noreply.github.com>
2022-03-04 20:06:19 +0500

    YAngo

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v*ecvx&rpudgjk5!+6)+uv$z91+0@15jly69^0a$759+2g1p)0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
